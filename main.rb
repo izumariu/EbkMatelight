a72812814a312c562dd48f93189cd8e5c59123d3
sesshomariu <sesshotv@t-online.de>
2016-11-26 13:21:16 +0100

    Fixed something for debugging

--- main.rb
+++ main.rb
@@ -35,7 +35,7 @@ class EbkMateCanvas
       @addresses.each_with_index do |y, indexy|
         y.each_with_index do |x, indexx|
           #@leds[@addresses[y][x]] = WS2812::Color.new(@leds[@addresses[y][x]].to_s(16)[0,2].to_i(16))
-          p @leds[@addresses[y][x]]
+          p @canvas[@addresses[y][x]]
         end
       end
     else
