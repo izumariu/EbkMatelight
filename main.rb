da1064422734a57f5cce04663c90c69322e84f2a
sesshomariu <sesshotv@t-online.de>
2016-11-26 17:48:57 +0100

    debug9

--- main.rb
+++ main.rb
@@ -21,7 +21,7 @@ class EbkMateCanvas
     @by = 8
     @by.times{ @canvas << Array.new; @bx.times{@canvas[-1] << 0} }
     @by.times{ @addresses << Array.new; @bx.times{@addresses[-1] << @@addrc; @@addrc+=1} }
-    @addresses.map!{|i|@addresses.index(i)%2==1;@addresses[@addresses.index(i)].reverse;i}
+    #@addresses.map!{|i|@addresses.index(i)%2==1;i.reverse;i}
     $RASPBIAN&&(@leds=Ws2812::Basic.new(40, 18); @leds.show)
   end
 
