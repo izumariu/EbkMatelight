5991e9f17b11855f913b5cc308509ccd1fcc3484
sesshomariu <sesshotv@t-online.de>
2016-11-26 18:05:15 +0100

    debug15

--- main.rb
+++ main.rb
@@ -97,7 +97,6 @@ queuewatch = Thread.new {
           when "\""
             5.times do |i|
               buf = []
-              p buf
               getFontChar(ch).each { |byte| buf << byte[i+1] }
               buf.map!(&:to_i)
               $CANVAS<<buf;$CANVAS.show;sleep 0.04
@@ -107,7 +106,6 @@ queuewatch = Thread.new {
           else
             8.times do |i|
               buf = []
-              p buf
               getFontChar(ch).each { |byte| buf << byte[i] }
               buf.map!(&:to_i)
               buf.all?(&0.method(:==))||($CANVAS<<buf;$CANVAS.show;sleep 0.04)
