27a9904a507bba04624593a885682f5391c4aab0
Hermlon <hermlon@t-online.de>
2016-12-10 19:25:02 +0100

    Javascript dynamic viewpager

--- gui_clients/BottleLightsWeb/scripts/main/pager.js
+++ gui_clients/BottleLightsWeb/scripts/main/pager.js
@@ -1,9 +1,22 @@
 function Pager(root) {
   this.names = [];
+  this.divs = [];
 
   this.add = function(name, obj) {
-    this.names[this.names.lenght] = name;
-    var x = document.createTextNode(this.names[this.names.lenght]);
-    root.appendChild(x);
+    this.names[this.names.length] = name;
+    this.update();
+  }
+  this.update = function() {
+    console.log("upd8" + this.names.length);
+    root.innerHTML = "";
+    var maindiv = document.createElement("div");
+    maindiv.setAttribute("style", "background-color: green;");
+    for(i = 0; i < this.names.length; i ++) {
+      this.divs[i] = document.createElement("div");
+      this.divs[i].setAttribute("style", "display: inline-block; width: " + ((100 / this.names.length)) + "%;");
+      this.divs[i].appendChild(document.createTextNode(this.names[i]));
+      maindiv.appendChild(this.divs[i]);
+    }
+    root.appendChild(maindiv);
   }
 }
--- gui_clients/BottleLightsWeb/scripts/mainscript.js
+++ gui_clients/BottleLightsWeb/scripts/mainscript.js
@@ -3,6 +3,5 @@ function init() {
   var pager = new Pager(document.body);
   pager.add("Test1");
   pager.add("Test2");
-
-  console.log(pager.name);
+  pager.add("Test3");
 }
