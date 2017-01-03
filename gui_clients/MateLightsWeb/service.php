474ebd16fd970597bce3ef97cd7d0c8bfef2caa7
Hermlon <hermlon@t-online.de>
2017-01-03 21:34:36 +0100

    Sending text to telnet should work but is not tested yet.

--- gui_clients/MateLightsWeb/service.php
+++ gui_clients/MateLightsWeb/service.php
@@ -1,7 +1,13 @@
 <?php
+  const ip = "localhost";
+  const port = "80";
+
   if($_GET["text"] != "") {
     if(substr($_GET["text"], 0, 2) != "d=") {
-      echo "sucess";
+      if(sendTelnet($_GET["text"])) {
+        echo "sucess";
+      }
+      /*Specific error message by the sendTelnet function*/
     }
     else {
       echo "Chronos allein bestimmt über die Zeit! (Eigentlich habe ich nichts für griechische Mythologie übrig...)";
@@ -9,6 +15,32 @@
   }
   else {
     echo "Von nichts kommt nichts!";
+  }
+
+  function sendTelnet($msg) {
+    /*Create the socket*/
+    $socket = socket_create(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    if(!$socket) {
+      $errorcode = socket_last_error();
+      $errormsg = socket_strerror($errorcode);
+      echo "[ERROR] Could not create socket: [$errorcode] $errormsg";
+      return false;
+    }
+    /*Connect to the socket*/
+    if(!socket_connect($socket, ip, port)) {
+      $errorcode = socket_last_error();
+      $errormsg = socket_strerror($errorcode);
+      echo "[ERROR] Could not connect to server: [$errorcode] $errormsg";
+      return false;
+    }
+    /*Send the message*/
+    if(!socket_send($socket, $msg, strlen($msg), 0)) {
+      $errorcode = socket_last_error();
+      $errormsg = socket_strerror($errorcode);
+      echo "[ERROR] Could not send message: [$errorcode] $errormsg";
+      return false;
+    }
+    return true;
   }
 
 ?>
